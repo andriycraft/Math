176f83ca285eb8bef3985d3d4adadefbdf300276
VaxPex <97077459+VaxPex@users.noreply.github.com>
2022-04-01 08:56:11 +0300

    Add files via upload

---
+++ src/math/Vector2.js
@@ -0,0 +1,127 @@
+class Vector2 {
+    x;
+    y;
+
+    /**
+     * @param {number} x 
+     * @param {number} y 
+     */
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+    }
+
+    static zero(){
+        return new Vector2(0, 0);
+    }
+
+    /**
+     * @returns {number}
+     */
+    getX(){
+        return this.x;
+    }
+
+    /**
+     * @returns {number}
+     */
+    getY(){
+        return this.y;
+    }
+
+    /**
+     * @returns {number}
+     */
+    getFloorX(){
+        return Math.floor(this.x);
+    }
+
+    /**
+     * @returns {number}
+     */
+    getFloorY(){
+        return Math.floor(this.y);
+    }
+
+    /**
+     * @param {number} x 
+     * @param {number} y 
+     * @returns {Vector2}
+     */
+    add(x, y){
+        return new Vector2(this.x + x, this.y + y);
+    }
+
+    /**
+     * @param {Vector2} vector2 
+     * @returns {Vector2}
+     */
+    addVector(vector2){
+        return this.add(vector2.x, vector2.y);
+    }
+
+    /**
+     * @param {number} x 
+     * @param {number} y 
+     * @returns {Vector2}
+     */
+    subtract(x, y){
+        return this.add(-x, -y);
+    }
+
+    /**
+     * @param {number} vector2 
+     * @returns {Vector2}
+     */
+    subtractVector(vector2){
+        return this.add(-vector2.x, -vector2.y);
+    }
+
+    /**
+     * @param {number} number 
+     * @returns {Vector2}
+     */
+    multiply(number){
+        return new Vector2(this.x * number, this.y * number);
+    }
+
+    /**
+     * @param {number} number 
+     * @returns {Vector2}
+     */
+    divide(number){
+        return new Vector2(this.x / number, this.y / number);
+    }
+
+    /**
+     * @returns {Vector2}
+     */
+    ceil(){
+        return new Vector2(Math.ceil(this.x), Math.ceil(this.y));
+    }
+
+    /**
+     * @returns {Vector2}
+     */
+    floor(){
+        return new Vector2(Math.floor(this.x), Math.floor(this.y));
+    }
+
+    /**
+     * @param {Vector2} pos 
+     * @returns {number}
+     */
+    distance(pos){
+        return Math.sqrt(this.distanceSquared(pos));
+    }
+
+    /**
+     * @param {Vector2} pos 
+     * @returns {number}
+     */
+    distanceSquared(pos){
+        return ((this.x - pos.x) ** 2) + ((this.y - pos.y));
+    }
+}
+
+module.exports = Vector2;
---
+++ src/math/Vector3.js
@@ -0,0 +1,152 @@
+class Vector3 {
+    x;
+    y;
+    z;
+
+    /**
+     * 
+     * @param {number} x 
+     * @param {number} y 
+     * @param {number} z 
+     */
+    constructor(x, y, z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    /**
+     * @returns {Vector3}
+     */
+    static zero(){
+        return new Vector3(0, 0, 0);
+    }
+
+    /**
+     * @returns {number}
+     */
+    getX(){
+        return this.x;
+    }
+
+    /**
+     * @returns {number}
+     */
+    getY(){
+        return this.y;
+    }
+
+    /**
+     * @returns {number}
+     */
+    getZ(){
+        return this.z;
+    }
+
+    /**
+     * @returns {number}
+     */
+    getFloorX(){
+        return Math.floor(this.x);
+    }
+
+    /**
+     * @returns {number}
+     */
+    getFloorY(){
+        return Math.floor(this.y);
+    }
+
+    /**
+     * @returns {number}
+     */
+    getFloorZ(){
+        return Math.floor(this.z);
+    }
+
+    /**
+     * @param {number} x 
+     * @param {number} y 
+     * @param {number} z 
+     * @returns {Vector3}
+     */
+    add(x, y, z){
+        return new Vector3(this.x + x, this.y + y, this.z + z);
+    }
+
+    /**
+     * @param {Vector3} vector3 
+     * @returns {Vector3}
+     */
+    addVector(vector3){
+        return this.add(vector3.x, vector3.y, vector3.z);
+    }
+
+    /**
+     * @param {number} x 
+     * @param {number} y 
+     * @param {number} z 
+     * @returns {Vector3}
+     */
+    subtract(x, y, z){
+        return this.add(-x, -y, -z);
+    }
+
+    /**
+     * @param {Vector3} vector3 
+     * @returns {Vector3}
+     */
+    subtractVector(vector3){
+        return this.add(vector3.x, vector3.y, vector3.z);
+    }
+
+    /**
+     * 
+     * @param {number} number 
+     * @returns {Vector3}
+     */
+    multiply(number){
+        return new Vector3(this.x * number, this.y * number, this.z * number);
+    }
+
+    /**
+     * 
+     * @param {number} number 
+     * @returns {Vector3}
+     */
+    divide(number){
+        return new Vector3(this.x / number, this.y / number, this.z / number);
+    }
+
+    /**
+     * @returns {Vector3}
+     */
+    ceil(){
+        return new Vector3(Math.ceil(this.x), Math.ceil(this.y), Math.ceil(this.z));
+    }
+
+    /**
+     * @returns {Vector3}
+     */
+    floor(){
+        return new Vector3(Math.floor(this.x), Math.floor(this.y), Math.floor(this.z));
+    }
+
+    /**
+     * @param {Vector3} pos 
+     * @returns {number}
+     */
+    distance(pos){
+        return Math.sqrt(this.distanceSquared(pos));
+    }
+
+    /**
+     * @param {Vector3} pos 
+     * @returns {number}
+     */
+    distanceSquared(pos){
+        return ((this.x - pos.x) ** 2) + ((this.y - pos.y) ** 2) + ((this.z - pos.z) ** 2);
+    }
+}
+
+module.exports = Vector3;
