5697261f1696bf82cfe51eb60c1ac91bcebdedef
VaxPex <97077459+VaxPex@users.noreply.github.com>
2022-04-01 09:10:27 +0300

    Create Math.js

---
+++ src/Math.js
@@ -0,0 +1,19 @@
+/******************************************\
+ *  ____  _            ____  _         _  *
+ * | __ )| |_   _  ___| __ )(_)_ __ __| | *
+ * |  _ \| | | | |/ _ \  _ \| | '__/ _` | *
+ * | |_) | | |_| |  __/ |_) | | | | (_| | *
+ * |____/|_|\__,_|\___|____/|_|_|  \__,_| *
+ *                                        *
+ * This file is licensed under the GNU    *
+ * General Public License 3. To use or    *
+ * modify it you must accept the terms    *
+ * of the license.                        *
+ * ___________________________            *
+ * \ @author BlueBirdMC Team /            *
+\******************************************/
+
+const Vector3 = require("./Vector3");
+const Vector2 = require("./Vector2");
+
+module.exports = { Vector3, Vector2 };
